fc2650842297270be2326663d4d8699d6ddeaba3
Kay-Vogas <kay.vogas@gmail.com>
2025-12-15 22:32:35 -0300

    Backend atualização :Iniciando Cadastro User (Hash)

---
+++ back-end/src/main/java/com/fichaexpress/back_end/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.fichaexpress.back_end.controller;
+
+import com.fichaexpress.back_end.entities.User;
+import com.fichaexpress.back_end.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+@CrossOrigin(origins = "*")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+
+    @PostMapping("/casdatro")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User cadastrarUser(@RequestBody User user){
+        return userService.CadastrarUser(user);
+    }
+
+}
---
+++ back-end/src/main/java/com/fichaexpress/back_end/entities/FichaAbyssal.java
@@ -0,0 +1,31 @@
+package com.fichaexpress.back_end.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class FichaAbyssal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //private User user;
+
+    private String personagem;
+
+
+    private Integer atributoAgilidade;
+    private Integer atributoForca;
+    private Integer atributoPresenca;
+    private Integer atributoInteligencia;
+    private Integer atributoVigor;
+
+    private String classeCombatente;
+    private String classeEspecialista;
+    private String classeOcultista;
+
+
+}
